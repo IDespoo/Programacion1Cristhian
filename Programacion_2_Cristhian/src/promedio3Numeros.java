07d68acf32a7244f36a95c29d636c832c985392b
iDespoo <esniper3000@gmail.com>
2023-03-28 19:50:05 -0500

    Calcuala el promedio de 3 numeros

--- Programacion_2_Cristhian/src/promedio3Numeros.java
+++ Programacion_2_Cristhian/src/promedio3Numeros.java
@@ -5,14 +5,18 @@ public class promedio3Numeros
     {
 
 
-        int n,p=0;
+    double n,p=0;
+    int notas;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese tres numeros para sacarle el promedio");
+        System.out.println("Ingrese el numero de notas a las que desea sacar el promedio");
+        notas = sc.nextInt();
 
-        for (int i = 0; i <= 2; i++) {
-            n = sc.nextInt();
-            p = n + p;
-        }
+            for (int i = 0; i < notas; i++)
+            {
+                System.out.println("Ingrese la nota");
+                n = sc.nextDouble();
+                p = n + p;
+            }
 
         System.out.println("El promedio es = " + p / 3);
     }
